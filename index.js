7f973b779cd53f7c78b0f612e04bb9d023c6de97
Yogi-123 <myogi.jp@gmail.com>
2020-09-03 15:51:11 +0700

    tugas prototype

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// method string reverse
 String.prototype.reverse = function (n) {
     let hasil = ""
     for (let i = this.length; i >=0; i--) {
@@ -6,4 +7,19 @@ String.prototype.reverse = function (n) {
     return hasil
 }
 
-console.log("yogi".reverse())
+
+
+// method number isprima()
+
+Number.prototype.isprima = function (n) {
+    if (n==1 || n==2 || n==3 || n==5 || n==7) {
+        console.log(true)
+    } else if (n%2==0 || n%3==0 || n%5==0 || n%7==0) {
+        console.log(false)
+    } else {
+        console.log(true)
+    }
+}
+
+let a = 31
+a.isprima();
